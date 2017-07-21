049bdeda2bd4bfb29a82bedbdc3ff2f559e57311
Junyi Zhang <zjywill@hotmail.com>
2017-07-21 12:09:41 +0800

    home page

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -1,20 +1,21 @@
 package com.example
 
 import org.jetbrains.ktor.application.Application
-import org.jetbrains.ktor.application.call
 import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.content.files
+import org.jetbrains.ktor.content.static
 import org.jetbrains.ktor.features.DefaultHeaders
-import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.response.respondText
-import org.jetbrains.ktor.routing.Routing
-import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
 
 fun Application.main() {
     install(DefaultHeaders)
-    install(CallLogging)
-    install(Routing) {
-        get("/") {
-            call.respondText("Hello, World! mmmm")
+    routing {
+        static("static") {
+            files("content/images")
+        }
+        static("/") {
+            files("src/main/resources/web")
+            files("src/main/resources/web/index.html")
         }
     }
 }
